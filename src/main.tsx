9c7cd07a4bc589127b3b8832b7bea2cfeb34d09f
rownlvh8875-coder <rownlvh8875@gmail.com>
2026-09-10 17:31:16 +0900

    feat: load calendar workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/tokens.css';
 import './styles/app.css';
+import './styles/calendar.css';
 
 const rootElement = document.getElementById('root');
 
