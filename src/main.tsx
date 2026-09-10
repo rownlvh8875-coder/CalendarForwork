ea0c32fbe47ec3694a8c5682769392ad40226f93
rownlvh8875-coder <rownlvh8875@gmail.com>
2026-09-10 17:27:03 +0900

    feat: load CalendarForwork visual system

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
+import './styles/tokens.css';
+import './styles/app.css';
 
 const rootElement = document.getElementById('root');
 
