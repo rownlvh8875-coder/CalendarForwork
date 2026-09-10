0d1d0d5d73cc2d24b44c75cfe51fd83bf4eb6233
rownlvh8875-coder <rownlvh8875@gmail.com>
2026-09-10 17:36:35 +0900

    feat: load event panel styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App';
 import './styles/tokens.css';
 import './styles/app.css';
 import './styles/calendar.css';
+import './styles/events.css';
 
 const rootElement = document.getElementById('root');
 
