7a3a185ae1489c8994c3c21194ecc5b1fd5e776e
rownlvh8875-coder <rownlvh8875@gmail.com>
2026-09-11 13:25:58 +0900

    test: define project timeline command contract

--- src-tauri/src/commands_contract_tests.rs
+++ src-tauri/src/commands_contract_tests.rs
@@ -5,13 +5,14 @@ use crate::{
     commands::{
         clients::{clients_create, clients_get, clients_list, clients_remove, clients_replace},
         events::{
-            events_create, events_get, events_list_between, events_list_upcoming, events_remove,
-            events_replace,
+            events_create, events_get, events_list_between, events_list_by_project,
+            events_list_upcoming, events_remove, events_replace,
         },
         projects::{
             project_stages_list, projects_create, projects_get, projects_list,
             projects_replace, projects_set_archived,
         },
+        timeline::project_stage_history_list,
     },
 };
 
@@ -25,6 +26,7 @@ fn app_database_path_uses_calendarforwork_filename() {
 fn event_command_entrypoints_are_exposed() {
     let _ = events_list_between;
     let _ = events_list_upcoming;
+    let _ = events_list_by_project;
     let _ = events_get;
     let _ = events_create;
     let _ = events_replace;
@@ -45,4 +47,9 @@ fn master_command_entrypoints_are_exposed() {
     let _ = projects_create;
     let _ = projects_replace;
     let _ = projects_set_archived;
+}
+
+#[test]
+fn project_timeline_command_entrypoint_is_exposed() {
+    let _ = project_stage_history_list;
 }
